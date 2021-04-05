642400bfb48aab08f093270ed9649ca0cf0df9d3
Emre Utku Uyguç <emreuyguc@gmail.com>
2021-04-05 09:44:02 +0300

    model update

--- euu_boilerplate.php
+++ euu_boilerplate.php
@@ -26,198 +26,198 @@ require_once 'hooks/Template.hooks.php';
 
 class euu_boilerplate extends Module implements euu_boilerplate\ModuleConfig
 {
-    use euu_boilerplate\ModuleHooks;
-    use euu_boilerplate\ConfigurationController;
-    use euu_boilerplate\DebugUtility;
+	use euu_boilerplate\ModuleHooks;
+	use euu_boilerplate\ConfigurationController;
+	use euu_boilerplate\DebugUtility;
 
-    #USE HOOKS START
-    use euu_boilerplate\TemplateHooks;
+	#USE HOOKS START
+	use euu_boilerplate\TemplateHooks;
 
-    #USE HOOKS USE END
+	#USE HOOKS USE END
 
 
-    protected $_errors = [];
+	protected $_errors = [];
 
-    public function __construct()
-    {
-        $this->name = self::NAME;
-        $this->tab = self::TAB;
-        $this->version = self::VERSION;
-        $this->author = self::AUTHOR;
-        $this->need_instance = self::NEED_INSTANCE;
+	public function __construct()
+	{
+		$this->name = self::NAME;
+		$this->tab = self::TAB;
+		$this->version = self::VERSION;
+		$this->author = self::AUTHOR;
+		$this->need_instance = self::NEED_INSTANCE;
 
-        $this->bootstrap = self::USE_BOOTSTRAP;
+		$this->bootstrap = self::USE_BOOTSTRAP;
 
-        parent::__construct();
+		parent::__construct();
 
-        $this->displayName = $this->l(self::DISPLAY_NAME);
-        $this->description = $this->l(self::DESCRIPTION);
-        $this->confirmUninstall = $this->l(self::UNINSTALL_MSG);
+		$this->displayName = $this->l(self::DISPLAY_NAME);
+		$this->description = $this->l(self::DESCRIPTION);
+		$this->confirmUninstall = $this->l(self::UNINSTALL_MSG);
 
-        $this->controllers = self::CONTROLLERS;
+		$this->controllers = self::CONTROLLERS;
 
-        $this->ps_versions_compliancy = array('min' => self::MIN_PS_VERSION, 'max' => self::MAX_PS_VERSION);
-    }
+		$this->ps_versions_compliancy = array('min' => self::MIN_PS_VERSION, 'max' => self::MAX_PS_VERSION);
+	}
 
-    private function postProcess()
-    {
-        foreach (Tools::getAllValues() as $key => $value) {
-            $method_name = 'action' . ucfirst($key);
-            if (method_exists($this, $method_name)) {
-                $this->$method_name();
-            }
-        }
-    }
+	private function postProcess()
+	{
+		if(Tools::getIsset('action')){
+			$method_name = 'process' . ucfirst(Tools::getValue('action'));
+			if (method_exists($this, $method_name)) {
+				$this->$method_name();
+			}
+		}
+	}
 
-    public function enable($force_all = false)
-    {
-        return
-            parent::enable($force_all)
-            && $this->_installTabs();
-    }
+	public function enable($force_all = false)
+	{
+		return
+			parent::enable($force_all)
+			&& $this->_installTabs();
+	}
 
-    public function disable($force_all = false)
-    {
-        return
-            parent::disable($force_all)
-            && $this->_uninstallTabs();
-    }
+	public function disable($force_all = false)
+	{
+		return
+			parent::disable($force_all)
+			&& $this->_uninstallTabs();
+	}
 
-    public function install()
-    {
-        return
-            $this->_callModuleHook('_beforeInstall')
-            && parent::install()
-            && $this->_installTabs()
-            && $this->_registerHooks()
-            && $this->_initDefaultConfigurationValues()
-            && $this->_callModuleHook('_afterInstall');
-    }
+	public function install()
+	{
+		return
+			$this->_callModuleHook('_beforeInstall')
+			&& parent::install()
+			&& $this->_installTabs()
+			&& $this->_registerHooks()
+			&& $this->_initDefaultConfigurationValues()
+			&& $this->_callModuleHook('_afterInstall');
+	}
 
-    public function uninstall()
-    {
-        return
-            $this->_callModuleHook('_beforeUninstall')
-            && $this->_uninstallTabs()
-            && $this->_unregisterHooks()
-            && $this->_deleteConfigurationValues()
-            && parent::uninstall()
-            && $this->_callModuleHook('_afterUninstall');
-    }
+	public function uninstall()
+	{
+		return
+			$this->_callModuleHook('_beforeUninstall')
+			&& $this->_uninstallTabs()
+			&& $this->_unregisterHooks()
+			&& $this->_deleteConfigurationValues()
+			&& parent::uninstall()
+			&& $this->_callModuleHook('_afterUninstall');
+	}
 
-    private function _callModuleHook($hook)
-    {
-        if (method_exists($this, $hook)) {
-            $this->{$hook}();
-        }
-        return true;
-    }
+	private function _callModuleHook($hook)
+	{
+		if (method_exists($this, $hook)) {
+			$this->{$hook}();
+		}
+		return true;
+	}
 
-    private function _registerHooks()
-    {
-        foreach (self::USE_HOOKS as $hook) {
-            $this->registerHook($hook);
-        }
-        return true;
-    }
+	private function _registerHooks()
+	{
+		foreach (self::USE_HOOKS as $hook) {
+			$this->registerHook($hook);
+		}
+		return true;
+	}
 
-    private function _unregisterHooks()
-    {
-        foreach (self::USE_HOOKS as $hook) {
-            $this->unregisterHook($hook);
-        }
-        return true;
-    }
+	private function _unregisterHooks()
+	{
+		foreach (self::USE_HOOKS as $hook) {
+			$this->unregisterHook($hook);
+		}
+		return true;
+	}
 
-    private function _initDefaultConfigurationValues()
-    {
-        foreach (self::DEFAULT_CONFIGURATION as $key => $value) {
-            $db_key = self::NAME . '_' . $key;
-            if (self::DEFAULT_CONFIGURATION[$key] != Configuration::get($db_key)) {
-                Configuration::updateValue($db_key, $value);
-            }
-        }
-        return true;
-    }
+	private function _initDefaultConfigurationValues()
+	{
+		foreach (self::DEFAULT_CONFIGURATION as $key => $value) {
+			$db_key = self::NAME . '_' . $key;
+			if (self::DEFAULT_CONFIGURATION[$key] != Configuration::get($db_key)) {
+				Configuration::updateValue($db_key, $value);
+			}
+		}
+		return true;
+	}
 
-    private function _deleteConfigurationValues()
-    {
-        foreach (self::DEFAULT_CONFIGURATION as $key => $value) {
-            $db_key = self::NAME . '_' . $key;
-            Configuration::deleteByName($db_key);
-        }
-        return true;
-    }
+	private function _deleteConfigurationValues()
+	{
+		foreach (self::DEFAULT_CONFIGURATION as $key => $value) {
+			$db_key = self::NAME . '_' . $key;
+			Configuration::deleteByName($db_key);
+		}
+		return true;
+	}
 
-    private function _installTabs($tabs = null, $parent_tab_id = 0)
-    {
-        $tabs = $tabs ?? self::TABS;
+	private function _installTabs($tabs = null, $parent_tab_id = 0)
+	{
+		$tabs = $tabs ?? self::TABS;
 
-        foreach ($tabs as $index => $defined_tab) {
+		foreach ($tabs as $index => $defined_tab) {
 
-            $id_tab = (int)Tab::getIdFromClassName($defined_tab['controller']);
+			$id_tab = (int)Tab::getIdFromClassName($defined_tab['controller']);
 
-            if (!$id_tab) {
-                $id_tab = null;
-            }
+			if (!$id_tab) {
+				$id_tab = null;
+			}
 
-            $tab = new Tab($id_tab);
-            $tab->class_name = $defined_tab['controller'];
-            foreach (Language::getLanguages() as $lang) {
-                $tab->name[$lang['id_lang']] = $defined_tab['title'];
-            }
+			$tab = new Tab($id_tab);
+			$tab->class_name = $defined_tab['controller'];
+			foreach (Language::getLanguages() as $lang) {
+				$tab->name[$lang['id_lang']] = $defined_tab['title'];
+			}
 
-            $tab->id_parent = isset($defined_tab['parent_tab']) ? (int)Tab::getIdFromClassName($defined_tab['parent_tab']) : $parent_tab_id;
-            $tab->position = $defined_tab['position'] ?? Tab::getNewLastPosition($tab->id_parent);
-            $tab->icon = $defined_tab['icon'] ?? null;
+			$tab->id_parent = isset($defined_tab['parent_tab']) ? (int)Tab::getIdFromClassName($defined_tab['parent_tab']) : $parent_tab_id;
+			$tab->position = $defined_tab['position'] ?? Tab::getNewLastPosition($tab->id_parent);
+			$tab->icon = $defined_tab['icon'] ?? null;
 
-            $tab->active = 1;
-            $tab->module = $this->name;
-            try {
-                $tab->save();
-            } catch (Exception $e) {
-                $this->_errors[] = $e->getMessage();
-                if (isset($defined_tab['tabs'])) {
-                    break;
-                }
-            }
+			$tab->active = 1;
+			$tab->module = $this->name;
+			try {
+				$tab->save();
+			} catch (Exception $e) {
+				$this->_errors[] = $e->getMessage();
+				if (isset($defined_tab['tabs'])) {
+					break;
+				}
+			}
 
-            if (isset($defined_tab['tabs'])) {
-                $this->_installTabs($defined_tab['tabs'], $tab->id);
-            }
-        }
+			if (isset($defined_tab['tabs'])) {
+				$this->_installTabs($defined_tab['tabs'], $tab->id);
+			}
+		}
 
-        if (count($this->_errors) > 0) {
-            return false;
-        }
+		if (count($this->_errors) > 0) {
+			return false;
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    private function _uninstallTabs($tabs = null)
-    {
-        $tabs = $tabs ?? self::TABS;
+	private function _uninstallTabs($tabs = null)
+	{
+		$tabs = $tabs ?? self::TABS;
 
-        foreach ($tabs as $index => $defined_tab) {
-            $id_tab = (int)Tab::getIdFromClassName($defined_tab['controller']);
-            if ($id_tab) {
-                $tab = new Tab($id_tab);
-                try {
-                    $tab->delete();
-                } catch (Exception $e) {
-                    $this->_errors[] = $e->getMessage();
-                }
-            }
-            if (isset($defined_tab['tabs'])) {
-                $this->_uninstallTabs($defined_tab['tabs']);
-            }
-        }
+		foreach ($tabs as $index => $defined_tab) {
+			$id_tab = (int)Tab::getIdFromClassName($defined_tab['controller']);
+			if ($id_tab) {
+				$tab = new Tab($id_tab);
+				try {
+					$tab->delete();
+				} catch (Exception $e) {
+					$this->_errors[] = $e->getMessage();
+				}
+			}
+			if (isset($defined_tab['tabs'])) {
+				$this->_uninstallTabs($defined_tab['tabs']);
+			}
+		}
 
-        if (count($this->_errors) > 0) {
-            return false;
-        }
+		if (count($this->_errors) > 0) {
+			return false;
+		}
 
-        return true;
-    }
+		return true;
+	}
 
 }
--- helpers/DbModel.helper.php
+++ helpers/DbModel.helper.php
@@ -1,206 +1,262 @@
 <?php
-
-namespace euu_boilerplate;
+namespace euu_shippingintegration;
 
 if (!defined('_PS_VERSION_')) exit;
 
 use \Db;
 
-class DbModel extends \ObjectModel
-{
-    //todo : toggleStatus()
-    //todo : add enum
-    //todo : instance check table and schema
-    //todo : add force drop
+class DbModel extends \ObjectModel {
 
-    private static function _getTableName()
-    {
-        return (isset(static::$_PREFIX) && static::$_PREFIX == TRUE ? _DB_PREFIX_ . static::$definition['table'] : static::$definition['table']);
-    }
+	const TYPE_TEXT = 'text';
+	const TYPE_MEDIUMTEXT = 'mediumtext';
+	//todo : toggleStatus()
+	//todo : add enum
+	//todo : add default value
+	//todo : instance check table and schema
+	//todo : add force drop
+	private static function _getTableName() {
+		return (isset(static::$_PREFIX) && static::$_PREFIX == TRUE ? _DB_PREFIX_ . static::$definition['table'] : static::$definition['table']);
+	}
 
-    private static function _typeToSql($type)
-    {
-        switch ($type) {
-            case static::TYPE_INT:
-                return 'int';
-            case static::TYPE_STRING:
-                return 'varchar';
-            case static::TYPE_FLOAT:
-                return 'float';
-            case static::TYPE_DATE:
-                return 'datetime';
-            case static::TYPE_BOOL:
-                return 'tinyint(1)';
-        }
-    }
+	private static function _typeToSql($type) {
+		switch ($type) {
+			case static::TYPE_INT:
+				return 'int';
+			case static::TYPE_STRING:
+				return 'varchar';
+			case static::TYPE_FLOAT:
+				return 'float';
+			case static::TYPE_DATE:
+				return 'datetime';
+			case static::TYPE_BOOL:
+				return 'tinyint(1)';
+			case static::TYPE_TEXT;
+				return 'text';
+			case static::TYPE_MEDIUMTEXT;
+				return 'mediumtext';
+		}
+	}
 
-    private static function _makeColSqlFromDefination()
-    {
-        $col_sql = '';
-        foreach (static::$definition['fields'] as $col_name => $col_info) {
-            $col_sql .= "`{$col_name}` " .
-                static::_typeToSql($col_info['type']) .
-                (isset($col_info['size']) ? ($col_info['type'] == static::TYPE_BOOL ? '' : "({$col_info['size']})") : '') .
-                (isset($col_info['required']) && $col_info['required'] == true ? ' NOT NULL ' : '') .
-                (isset($col_info['auto_date']) ? ($col_info['auto_date'] == 'insert' ? ' DEFAULT CURRENT_TIMESTAMP '
-                    :
-                    ' ON UPDATE CURRENT_TIMESTAMP ') : '') .
-                ',';
-        }
-        return $col_sql;
-    }
+	private static function _makeColSqlFromDefination() {
+		$col_sql = '';
+		foreach (static::$definition['fields'] as $col_name => $col_info) {
+			$col_sql .= "`{$col_name}` " .
+						static::_typeToSql($col_info['type']) .
+						(isset($col_info['size']) ? ($col_info['type'] == static::TYPE_BOOL ? '' : "({$col_info['size']})") : '') .
+						(isset($col_info['required']) && $col_info['required'] == TRUE ? ' NOT NULL ' : '') .
+						(isset($col_info['auto_date']) ? ($col_info['auto_date'] == 'insert' ? ' DEFAULT CURRENT_TIMESTAMP ' : ' ON UPDATE CURRENT_TIMESTAMP ') : '') .
+						',';
+		}
 
-    private static function _makecolIndexFromDefination()
-    {
-        $index_sql = [];
-        foreach (static::$definition['fields'] as $col_name => $col_info) {
-            if (isset($col_info['uniq']) && $col_info['uniq'] == true) {
-                $index_sql[] = ",UNIQUE (`{$col_name}`)";
-            }
-        }
-        $index_sql = implode('', $index_sql);
+		return $col_sql;
+	}
 
-        return $index_sql;
-    }
+	private static function _makecolIndexFromDefination() {
+		$index_sql = [];
+		foreach (static::$definition['fields'] as $col_name => $col_info) {
+			if (isset($col_info['uniq']) && $col_info['uniq'] == TRUE) {
+				$index_sql[] = ",UNIQUE (`{$col_name}`)";
+			}
+		}
+		$index_sql = implode('', $index_sql);
 
-    public static function createTable()
-    {
-        $tableName = static::$definition['table'];
-        $primaryField = static::$definition['primary'];
-        $sql = '  			CREATE TABLE IF NOT EXISTS `' . (isset(static::$_PREFIX) && static::$_PREFIX == TRUE ? _DB_PREFIX_ . $tableName : $tableName) . '` (
-  			    `' . $primaryField . '` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  			    ' . static::_makeColSqlFromDefination() . '				
-  			    PRIMARY KEY (`' . $primaryField . '`)  				
-  			    ' . static::_makecolIndexFromDefination() . ' 			
-  			    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;  		';
+		return $index_sql;
+	}
 
-        return Db::getInstance()->execute($sql);
-    }
+	public static function createTable() {
+		$tableName = static::$definition['table'];
+		$primaryField = static::$definition['primary'];
+		$sql = '  			CREATE TABLE IF NOT EXISTS `' . (isset(static::$_PREFIX) && static::$_PREFIX == TRUE ? _DB_PREFIX_ . $tableName : $tableName) . '` (
+  				`' . $primaryField . '` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  				' . static::_makeColSqlFromDefination() . '				
+  				PRIMARY KEY (`' . $primaryField . '`)  				
+  				' . static::_makecolIndexFromDefination() . ' 			
+  				) ENGINE=InnoDB DEFAULT CHARSET=utf8;  		';
 
-    private function _reformatFields()
-    {
-        $autodate_fields = [];
+		return Db::getInstance()
+				 ->execute($sql);
+	}
 
-        foreach ($this->def['fields'] as $field_name => $field_param) {
-            if (isset($field_param['auto_date'])) {
-                $autodate_fields[] = $field_name;
-            }
-        }
+	private function _reformatFields() {
+		$autodate_fields = [];
+		foreach ($this->def['fields'] as $field_name => $field_param) {
+			if (isset($field_param['auto_date'])) {
+				$autodate_fields[] = $field_name;
+			}
+		}
+		$fields = $this->getFields();
+		array_walk(
+			$fields,
+			function ($field_value, $field_name) use (&$fields, &$autodate_fields) {
+				if (in_array($field_name, $autodate_fields)) {
+					unset($fields[$field_name]);
+				}
+			}
+		);
 
-        $fields = $this->getFields();
-        array_walk($fields, function ($field_value, $field_name) use (&$fields, &$autodate_fields) {
-            if (in_array($field_name, $autodate_fields)) {
-                unset($fields[$field_name]);
-            }
-        });
+		return $fields;
+	}
 
-        return $fields;
-    }
+	public function insert($null_values = TRUE, $force_id = FALSE, $insert_type = DB::INSERT) {
+		if (isset($this->id) && $force_id === FALSE) {
+			unset($this->id);
+		}
+		$fields = $this->_reformatFields();
+		if (!$result = Db::getInstance()
+						 ->insert(
+							 $this->def['table'],
+							 $fields,
+							 $null_values,
+							 FALSE,
+							 $insert_type,
+							 static::$_PREFIX
+						 )) {
+			return FALSE;
+		}
+		// Get object id in database
+		$this->id = Db::getInstance()
+					  ->Insert_ID();
 
-    public function insert($null_values = true, $force_id = false, $insert_type = DB::INSERT)
-    {
-        if (isset($this->id) && $force_id === false) {
-            unset($this->id);
-        }
+		return $result;
+	}
 
-        $fields = $this->_reformatFields();
+	public function update($null_values = TRUE) {
+		$fields = $this->_reformatFields();
+		if (!$result = Db::getInstance()
+						 ->update(
+							 $this->def['table'],
+							 $fields,
+							 '`' . pSQL($this->def['primary']) . '` = ' . (int)$this->id,
+							 0,
+							 $null_values,
+							 FALSE,
+							 Db::INSERT,
+							 static::$_PREFIX
+						 )) {
+			return FALSE;
+		}
 
-        if (!$result = Db::getInstance()->insert($this->def['table'], $fields, $null_values, FALSE, $insert_type, static::$_PREFIX)) {
-            return false;
-        }
+		return $result;
+	}
 
-        // Get object id in database
-        $this->id = Db::getInstance()->Insert_ID();
+	public function delete() {
+		if (!$result = Db::getInstance()
+						 ->delete(
+							 $this->def['table'],
+							 '`' . pSQL($this->def['primary']) . '` = ' . (int)$this->id,
+							 0,
+							 FALSE,
+							 static::$_PREFIX
+						 )) {
+			return FALSE;
+		}
 
-        return $result;
-    }
+		return $result;
+	}
 
-    public function update($null_values = true)
-    {
-        $fields = $this->_reformatFields();
+	// todo name change
+	public static function selectInit($where, $order_by = NULL): self {
+		$id = Db::getInstance()
+				->getValue(
+					'SELECT ' .
+					static::$definition['primary'] .
+					' FROM ' .
+					static::_getTableName() .
+					($where != NULL ? " WHERE {$where}" : '') .
+					($order_by != NULL ? ' ORDER BY ' . implode(
+							',',
+							array_map(
+								function ($column) {
+									return implode(' ', $column);
+								},
+								$order_by
+							)
+						) : '')
+				);
 
-        if (!$result = Db::getInstance()->update($this->def['table'], $fields, '`' . pSQL($this->def['primary']) . '` = ' . (int)$this->id, 0, $null_values, FALSE, Db::INSERT, static::$_PREFIX)) {
-            return false;
-        }
+		return new static($id);
+	}
 
-        return $result;
-    }
+	public static function selectAll(string $where = NULL, array $select_columns = NULL, array $order_by = NULL) {
+		return (Db::getInstance()
+				  ->executeS(
+					  'SELECT '.($select_columns ? '`'.implode('`,`',$select_columns).'`' : '*').' FROM ' . static::_getTableName() . ($where != NULL ? " WHERE {$where}" : '') . ($order_by != NULL ? ' ORDER BY ' . implode(
+							  ',',
+							  array_map(
+								  function ($column) {
+									  return implode(' ', $column);
+								  },
+								  $order_by
+							  )
+						  ) : '')
+				  ));
+	}
 
-    public function delete()
-    {
-        if (!$result = Db::getInstance()->delete($this->def['table'], '`' . pSQL($this->def['primary']) . '` = ' . (int)$this->id, 0, FALSE, static::$_PREFIX)) {
-            return false;
-        }
+	//todo : select columns active
+	public static function selectRow(string $where = NULL, array $select_columns = NULL, array $order_by = NULL) {
+		return (Db::getInstance()
+				  ->getRow(
+					  'SELECT '.($select_columns ? '`'.implode('`,`',$select_columns).'`' : '*').' FROM ' . static::_getTableName() . ($where != NULL ? " WHERE {$where}" : '') . ($order_by != NULL ? ' ORDER BY ' . implode(
+							  ',',
+							  array_map(
+								  function ($column) {
+									  return implode(' ', $column);
+								  },
+								  $order_by
+							  )
+						  ) : '')
+				  ));
+	}
 
-        return $result;
-    }
+	public static function selectValue(string $select_column, string $where = '1', array $order_by = NULL) {
+		return (Db::getInstance()
+				  ->getValue(
+					  'SELECT ' . $select_column . ' FROM ' . static::_getTableName() . " WHERE {$where} " . ($order_by != NULL ? ' ORDER BY ' . implode(
+							  ',',
+							  array_map(
+								  function ($column) {
+									  return implode(' ', $column);
+								  },
+								  $order_by
+							  )
+						  ) : '')
+				  ));
+	}
 
-    // todo name change
-    public static function selectInit($where, $order_by = NULL): self
-    {
-        $id = Db::getInstance()->getRow('SELECT * FROM ' . static::_getTableName() .
-            ($where != NULL ? " WHERE {$where}" : '') .
-            (
-            $order_by != null ? ' ORDER BY ' .
-                implode(',', array_map(function ($column) {
-                    return implode(' ', $column);
-                }, $order_by)) : ''
-            )
+	//note is
+	public static function check(string $where) {
+		return (Db::getInstance()
+				  ->getValue('SELECT COUNT(*) FROM ' . static::_getTableName() . " WHERE {$where}"));
+	}
 
-        )[static::$definition['primary']];
-        return new self($id);
-    }
+	public static function deleteRow(string $where) {
+		return (Db::getInstance()
+				  ->delete(static::$definition['table'], $where, 0, FALSE, static::$_PREFIX));
+	}
 
-    public static function selectAll(string $where = NULL, array $select_columns = NULL)
-    {
-        return (Db::getInstance()->executeS('SELECT * FROM ' . static::_getTableName() . ($where != NULL ? " WHERE {$where}" : '')));
-    }
+	public static function insertRow(array $fields, $insert_type = Db::INSERT) {
+		return (Db::getInstance()
+				  ->insert(
+					  static::$definition['table'],
+					  $fields,
+					  TRUE,
+					  FALSE,
+					  $insert_type,
+					  static::$_PREFIX
+				  ));
+	}
 
-    //todo : select columns active
-    public static function selectRow(string $where = NULL, array $select_columns = NULL, array $order_by = NULL)
-    {
-        return (Db::getInstance()->getRow('SELECT * FROM ' . static::_getTableName() .
-            ($where != NULL ? " WHERE {$where}" : '') .
-
-            ($order_by != null ? ' ORDER BY ' .
-                implode(',', array_map(function ($column) {
-                    return implode(' ', $column);
-                }, $order_by)) : ''
-            )
-
-        ));
-    }
-
-    public static function selectValue(string $select_column, string $where = '1', array $order_by = NULL)
-    {
-        return (Db::getInstance()->getValue('SELECT ' . $select_column . ' FROM ' .
-            static::_getTableName() .
-            " WHERE {$where} " .
-            ($order_by != null ? ' ORDER BY ' . implode(',', array_map(function ($column) {
-                    return implode(' ', $column);
-                }, $order_by)) : '')
-        ));
-    }
-
-    //note is
-    public static function check(string $where)
-    {
-        return (Db::getInstance()->getValue('SELECT COUNT(*) FROM ' . static::_getTableName() . " WHERE {$where}"));
-    }
-
-    public static function deleteRow(string $where)
-    {
-        return (Db::getInstance()->delete(static::$definition['table'], $where, 0, FALSE, static::$_PREFIX));
-    }
-
-    public static function insertRow(array $fields, $insert_type = Db::INSERT)
-    {
-        return (Db::getInstance()->insert(static::$definition['table'], $fields, TRUE, FALSE, $insert_type, static::$_PREFIX));
-    }
-
-    public static function updateRow(array $fields, string $where)
-    {
-        return (Db::getInstance()->update(static::$definition['table'], $fields, $where, 0, TRUE, FALSE, static::$_PREFIX));
-    }
+	public static function updateRow(array $fields, string $where) {
+		return (Db::getInstance()
+				  ->update(
+					  static::$definition['table'],
+					  $fields,
+					  $where,
+					  0,
+					  TRUE,
+					  FALSE,
+					  static::$_PREFIX
+				  ));
+	}
 
 }
